b278a94db90b68d9236d4dcfdc77a4b3c40af1d8
steven <pass2001@163.com>
2025-09-23 06:45:18 +0800

    2025923提交更新

--- wp-content/themes/ripro/inc/admin/page/vip.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('ABSPATH')) {die;} // Cannot access directly.
-date_default_timezone_set('Asia/Shanghai');
-
-?>
-<script type="text/javascript">
-　　window.location.href="<?php echo admin_url('/users.php'); ?>";
-</script>
